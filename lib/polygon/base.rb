29c301fd53cbaeaf50ebebb041bc7c7386ff8e1e
Bernard Lambeau <blambeau@gmail.com>
2012-06-11 11:11:52 +0200

    Remove old wlang support.

--- lib/polygon/base.rb
+++ lib/polygon/base.rb
@@ -2,11 +2,11 @@ module Polygon
   class Base < Sinatra::Base
 
     configure do
-      set :static,        Proc.new { root && root/:content/:static   }
-      set :public_folder, Proc.new { root && root/:content/:static   }
-      set :dynamic,       Proc.new { root && root/:content/:dynamic  }
-      set :templates,     Proc.new { root && root/:design/:templates }
-      set :views,         Proc.new { root && root/:design/:templates }
+      set :static,        Proc.new { root && root / 'content/static'   }
+      set :public_folder, Proc.new { root && root / 'content/static'   }
+      set :dynamic,       Proc.new { root && root / 'content/dynamic'  }
+      set :templates,     Proc.new { root && root / 'design/templates' }
+      set :views,         Proc.new { root && root / 'design/templates' }
       enable  :logging
       enable  :raise_errors
       disable :show_exceptions
