8f47192da589a615c9ae43ed53025359a524e2c1
Bernard Lambeau <blambeau@gmail.com>
2012-06-11 11:49:58 +0200

    Avoid hiding sinatra's :templates setting.

--- lib/polygon/base.rb
+++ lib/polygon/base.rb
@@ -5,7 +5,6 @@ module Polygon
       set :static,        Proc.new { root && root / 'content/static'   }
       set :public_folder, Proc.new { root && root / 'content/static'   }
       set :dynamic,       Proc.new { root && root / 'content/dynamic'  }
-      set :templates,     Proc.new { root && root / 'design/templates' }
       set :views,         Proc.new { root && root / 'design/templates' }
       enable  :logging
       enable  :raise_errors
