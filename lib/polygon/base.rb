c81ddef10431414298d4728637f628a68795656d
Bernard Lambeau <blambeau@gmail.com>
2012-06-12 17:27:24 +0200

    Refactor helpers and add a database instance.

--- lib/polygon/base.rb
+++ lib/polygon/base.rb
@@ -6,6 +6,7 @@ module Polygon
       set :public_folder, Proc.new { root && root / 'content/static'  }
       set :dynamic,       Proc.new { root && root / 'content/dynamic' }
       set :views,         Proc.new { root && root / 'design/views'    }
+      set :database,      Proc.new { Database.new(dynamic)            }
       enable  :logging
       enable  :raise_errors
       disable :show_exceptions
--- lib/polygon/helpers.rb
+++ lib/polygon/helpers.rb
@@ -1,9 +1,34 @@
 module Polygon
   module Helpers
 
-    def static;    settings.static;    end
-    def dynamic;   settings.dynamic;   end
-    def templates; settings.templates; end
+    def static
+      settings.static
+    end
+
+    def dynamic
+      settings.dynamic
+    end
+
+    def templates
+      settings.templates
+    end
+
+    def lispy(&bl)
+      @lispy ||= Alf.lispy(settings.database)
+      bl ? @lispy.evaluate(&bl) : @lispy
+    end
+
+    def in_production
+      settings.environment == :production
+    end
+
+    def in_test
+      settings.environment == :test
+    end
+
+    def in_development
+      settings.environment == :development
+    end
 
   end # module Helpers
 end # module Polygon
