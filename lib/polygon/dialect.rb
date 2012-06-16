4f3944823cf4bacc6c6b3c99c5319111bfa34e64
Bernard Lambeau <blambeau@gmail.com>
2012-06-16 17:17:00 +0200

    Missing file in previous commit.

---
+++ lib/polygon/dialect.rb
@@ -0,0 +1,13 @@
+module Polygon
+  class Dialect < WLang::Html
+
+    def tilde(buf, fn)
+      text = evaluate(fn)
+      buf << context.markdown(text)
+    end
+
+    tag '~', :tilde
+
+    Tilt.prefer Tilt::WLangTemplate.with_options(:dialect => Dialect), 'wlang'
+  end
+end
