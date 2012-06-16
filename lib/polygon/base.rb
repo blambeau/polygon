c3931c1959c66f331bedcb261456e4a1d26eec00
Bernard Lambeau <blambeau@gmail.com>
2012-06-16 12:13:09 +0200

    Add default rules for 404 and 500

--- lib/polygon/base.rb
+++ lib/polygon/base.rb
@@ -11,6 +11,14 @@ module Polygon
       disable :show_exceptions
     end
 
+    not_found do
+      send_file settings.public_folder + '/404.html', :status => 404
+    end
+
+    error do
+      send_file settings.public_folder + '/500.html', :status => 500
+    end
+
     helpers Polygon::Helpers
   end # Base
 end # module Polygon
