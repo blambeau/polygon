83f95329f99ba413ef2b218993257c0cf7384ea1
Bernard Lambeau <blambeau@gmail.com>
2012-06-17 12:36:30 +0200

    Add local helpers.

--- lib/polygon/helpers.rb
+++ lib/polygon/helpers.rb
@@ -34,5 +34,20 @@ module Polygon
       settings.environment == :development
     end
 
+    def sitemap_locals
+      { :urls => database.sitemap }
+    end
+
+    def page_locals(path = "")
+      rel = lispy do
+        (unwrap \
+          (extend (restrict :sitemap, :path => path),
+                  :data => lambda{ entry.to_hash }),
+          :data)
+      end
+      rel && rel.to_a.first
+    end
+    alias :index_locals :page_locals
+
   end # module Helpers
 end # module Polygon
